2bbfcbde68786c92e024688d88f62470395922f5
laraschmitt <lara_schmitt@posteo.de>
2020-10-15 22:48:02 +0200

    add get first coordinates

--- create_tweets_table.sql
+++ create_tweets_table.sql
@@ -1,22 +1,24 @@
 -- script to create a table for storing twitter tweets 
+
+DROP TABLE tweets_test;
+
 CREATE TABLE tweets_test(
-    id INT NOT NULL PRIMARY KEY,
-    timestamp_ms TIMESTAMP NOT NULL,
+    id BIGINT NOT NULL,
+    timestamp_ms VARCHAR NOT NULL,
     tweet VARCHAR NOT NULL,
     url_in_tweet VARCHAR,
+
     user_url VARCHAR,
     user_location VARCHAR, 
-    user_followers_count INT NOT NULl,
-    user_verified VARCHAR NOT NULL,
-    user_statuses_count INT NOT NULL,
+    user_followers_count BIGINT,
+    user_verified BOOLEAN,
+    user_statuses_count BIGINT,
+    user_id BIGINT,
+    user_created_at TIMESTAMP,
+
     lang VARCHAR(10),
-    geo VARCHAR,
-    coordinates VARCHAR,
-    place VARCHAR,
-    user_id INT NOT NULL,
-    user_created_at TIMESTAMP NOT NULL
+    from_coords VARCHAR,
+    from_country_coords VARCHAR,
+    to_country_coords VARCHAR,
+    tweet_country_code VARCHAR    
 );
-
-
--- create a column only for the data, how to insert?
-SELECT to_timestamp(1195374767);
