03978055b98d8858323884c18c37529f2f2a0615
laraschmitt <lara_schmitt@posteo.de>
2020-10-30 14:59:14 +0100

    add hashtag col

--- populate_db.py
+++ populate_db.py
@@ -36,6 +36,20 @@ def extract_url(urls) -> str:
     return found_url
 
 
+def extract_hashtag(hashtag_list):
+    user_hashtags = []
+    if hashtag_list:
+
+        for hashtag in hashtag_list:
+            hashtags_from_user = hashtag.get('text').lower()
+
+            if (not 'anonymus' in hashtags_from_user) and (not 'anonymous' in hashtags_from_user):
+
+                user_hashtags.append(hashtags_from_user)
+
+    return ', '.join(user_hashtags)
+
+
 # Declare postgres config
 HOST = 'localhost'
 USERNAME = 'postgres'
@@ -51,6 +65,7 @@ engine = create_engine(database_url)
 
 # function to load data into postgres
 
+# need: hashtags, city names, retweeted / upvotes
 
 def load(j):
     """ Loads transformed tweeets into the Postgres database
@@ -63,6 +78,7 @@ def load(j):
         tweet_id,
         timestamp_ms,
         tweet,
+        hashtags,
         url_in_tweet,
         user_url,
         user_location,
@@ -76,13 +92,15 @@ def load(j):
         user_loc_country_coords,
         from_country_coords,
         from_country_name,
+        from_city_name,
         to_country_coords,
-        to_country_name
+        to_country_name,
+        to_city_name
         )
         VALUES (%s, %s, %s, %s, %s,
                 %s, %s, %s, %s, %s,
                 %s, %s, %s, %s, %s,
-                %s, %s, %s)
+                %s, %s, %s, %s, %s, %s)
         ON CONFLICT (tweet_id) DO NOTHING;"""
 
     # check for valid entries and if tweet json contains
@@ -91,10 +109,11 @@ def load(j):
     user = j.get('user')
     user_location = j.get('user', {}).get('location')
     valid_entry = j.get('created_at')
-    place_attr = j.get('place')
+    #place_attr = j.get('place')
     user_url = j.get('user', {}).get('url')
     urls = j.get('entities', {}).get('urls')
     lang_code = j.get('lang')
+    hashtag_list = j.get('entities', {}).get('hashtags', [])
 
     # conditions to still be included !
     follower_c = j.get('user', {}).get('followers_count')
@@ -102,58 +121,15 @@ def load(j):
 
     if (valid_entry and len(urls) and user_url) and user_location and ((ver and follower_c > 50000) or follower_c > 100000):
 
+        hashtags = ''
         user_loc_city_coords = ''
         user_loc_country_coords = ''
         to_country_coords = ''
         to_country_name = ''
+        to_city_name = 'coming_soon'
         from_country_coords = ''
         from_country_name = ''
-
-        # loop through multilingual city list
-        for c_name_multi in df_city['CITY_NAME']:
-            # check if the found cityname in the tweet is a cityname in the language the tweet was written in (detected from twitter)
-            if c_name_multi in j['text'] and df_city.loc[df_city['CITY_NAME'] == c_name_multi]['language'].values[0] == lang_code:
-
-                print('-------City found in tweet---------', c_name_multi, ' language', lang_code)
-
-                # map the cityname to its countries coordinates
-                to_country_coords = df_city.loc[df_city['CITY_NAME']
-                                                == c_name_multi]['country_lat_long_3857'].values[0]
-                # get countrycode and then countryname
-                to_country_code = df_country.loc[df_country['country_lat_long_3857']
-                                                 == to_country_coords]['COUNTRY_ALPHA2_CODE'].values[0]
-                to_country_name = df_councode_counname_en.loc[df_councode_counname_en[
-                    'COUNTRY_ALPHA2_CODE'] == to_country_code]['COUNTRY_NAME'].values[0]
-
-                # find user loc coordinates and country coords
-                for _c_name_multi in df_city['CITY_NAME']:
-
-                    if _c_name_multi in user_location.lower():
-                        user_loc_city_coords = df_city.loc[df_city['CITY_NAME'] == _c_name_multi]['city_lat_long_3857'].values[0]
-                        from_country_coords = df_city.loc[df_city['CITY_NAME'] == _c_name_multi]['country_lat_long_3857'].values[0]
-                        from_country_code = df_country.loc[df_country['country_lat_long_3857']
-                                                           == from_country_coords]['COUNTRY_ALPHA2_CODE'].values[0]
-                        from_country_name = df_councode_counname_en.loc[df_councode_counname_en[
-                            'COUNTRY_ALPHA2_CODE'] == from_country_code]['COUNTRY_NAME'].values[0]
-
-                for _c_name_multi in df_country['COUNTRY_NAME']:
-
-                    if _c_name_multi in user_location.lower():
-
-                        user_loc_country_coords = df_country.loc[df_country['COUNTRY_NAME']
-                                                                 == _c_name_multi]['country_lat_long_3857'].values[0]
-                        from_country_coords = df_country.loc[df_country['COUNTRY_NAME']
-                                                             == _c_name_multi]['country_lat_long_3857'].values[0]
-                        from_country_code = df_country.loc[df_country['country_lat_long_3857']
-                                                           == from_country_coords]['COUNTRY_ALPHA2_CODE'].values[0]
-                        from_country_name = df_councode_counname_en.loc[df_councode_counname_en[
-                            'COUNTRY_ALPHA2_CODE'] == from_country_code]['COUNTRY_NAME'].values[0]
-
-                # print('City Loop', j['id'])
-                # print('Setting user_location', user_location)
-                # print('user_loc_city_coords', user_loc_city_coords)
-                # print('from_country_coords', from_country_coords)
-                # print('from_country_name', from_country_name)
+        from_city_name = 'coming_soon'
 
         # loop through multilingual country list
         for c_name_multi in df_country['COUNTRY_NAME']:
@@ -202,8 +178,57 @@ def load(j):
                         if not from_country_name:
                             from_country_name = df_councode_counname_en.loc[df_councode_counname_en['COUNTRY_ALPHA2_CODE'] == from_country_code]['COUNTRY_NAME'].values[0]
 
+                # loop through multilingual city list
+        for c_name_multi in df_city['CITY_NAME']:
+            # check if the found cityname in the tweet is a cityname in the language the tweet was written in (detected from twitter)
+            if c_name_multi in j['text'] and df_city.loc[df_city['CITY_NAME'] == c_name_multi]['language'].values[0] == lang_code:
+
+                print('-------City found in tweet---------', c_name_multi, ' language', lang_code)
+
+                # map the cityname to its countries coordinates
+                to_country_coords = df_city.loc[df_city['CITY_NAME']
+                                                == c_name_multi]['country_lat_long_3857'].values[0]
+                # get countrycode and then countryname
+                to_country_code = df_country.loc[df_country['country_lat_long_3857']
+                                                 == to_country_coords]['COUNTRY_ALPHA2_CODE'].values[0]
+                to_country_name = df_councode_counname_en.loc[df_councode_counname_en[
+                    'COUNTRY_ALPHA2_CODE'] == to_country_code]['COUNTRY_NAME'].values[0]
+
+                # find user loc coordinates and country coords
+                for _c_name_multi in df_city['CITY_NAME']:
+
+                    if _c_name_multi in user_location.lower():
+                        user_loc_city_coords = df_city.loc[df_city['CITY_NAME'] == _c_name_multi]['city_lat_long_3857'].values[0]
+                        from_city_name = df_city.loc[df_city['CITY_NAME'] == _c_name_multi]['CITY_NAME'].values[0]
+                        from_country_coords = df_city.loc[df_city['CITY_NAME'] == _c_name_multi]['country_lat_long_3857'].values[0]
+                        from_country_code = df_country.loc[df_country['country_lat_long_3857']
+                                                           == from_country_coords]['COUNTRY_ALPHA2_CODE'].values[0]
+                        from_country_name = df_councode_counname_en.loc[df_councode_counname_en[
+                            'COUNTRY_ALPHA2_CODE'] == from_country_code]['COUNTRY_NAME'].values[0]
+
+                for _c_name_multi in df_country['COUNTRY_NAME']:
+
+                    if _c_name_multi in user_location.lower():
+
+                        user_loc_country_coords = df_country.loc[df_country['COUNTRY_NAME']
+                                                                 == _c_name_multi]['country_lat_long_3857'].values[0]
+                        from_country_coords = df_country.loc[df_country['COUNTRY_NAME']
+                                                             == _c_name_multi]['country_lat_long_3857'].values[0]
+                        from_country_code = df_country.loc[df_country['country_lat_long_3857']
+                                                           == from_country_coords]['COUNTRY_ALPHA2_CODE'].values[0]
+                        from_country_name = df_councode_counname_en.loc[df_councode_counname_en[
+                            'COUNTRY_ALPHA2_CODE'] == from_country_code]['COUNTRY_NAME'].values[0]
+
+                # print('City Loop', j['id'])
+                # print('Setting user_location', user_location)
+                # print('user_loc_city_coords', user_loc_city_coords)
+                # print('from_country_coords', from_country_coords)
+                # print('from_country_name', from_country_name)
+
                 # get url mentionend in tweet from entities dict
                 found_url = extract_url(j.get('entities').get('urls'))
+
+                hashtags = extract_hashtag(hashtag_list)
 
                 # print('Country Loop', j['id'])
                 # print('Setting user_location', user_location)
@@ -217,6 +242,7 @@ def load(j):
                         j['id'],  # id
                         j['timestamp_ms'],  # timestamp_ms
                         j['text'],  # tweet
+                        hashtags,
                         found_url,  # url_in_tweet
                         user['url'],  # user_url
                         user_location,  # user_location
@@ -231,8 +257,10 @@ def load(j):
                         user_loc_country_coords,
                         from_country_coords,
                         from_country_name,
+                        from_city_name,
                         to_country_coords,
-                        to_country_name
+                        to_country_name,
+                        to_city_name
                     ))
 
                     print('------------------------------------> TWEET INGESTED IN DATABASE---------')
